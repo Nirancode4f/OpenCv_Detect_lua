1031fdfb485551baaa246eb1313527c71df95aad
OpenCv_python_detect_lua <khgammingcraft@gmail.com>
2022-04-02 10:42:49 +0700

    "update test"

---
+++ main.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+import cv2 as cv
+from resize_img import resize as rs
+
+
+# import img here
+img = "./Test_Data/lua.jpg"
+img = cv.imread(img)
+
+# run this
+def main(img):
+
+
+    img = rs(img)
+
+    cv.imshow("nothing", img)
+
+    cv.waitKey(0)
+
+
+
+
+if _name_ == "_name_":
+    main(img)
---
+++ resize_img.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+
+
+def resize(img):
+
+    resized = cv.resize(img, (800,800))
+    return resized
+
+
+
